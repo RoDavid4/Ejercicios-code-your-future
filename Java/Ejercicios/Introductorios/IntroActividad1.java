3209257c7775f5b19d4e1a3c889a621a1ace9f8d
David Rodríguez <rdavid.97355@gmail.com>
2024-10-02 20:52:25 -0300

    se agregan los ejercicios de la clase de introduccion

---
+++ Java/Ejercicios/Introductorios/IntroActividad1.java
@@ -0,0 +1,16 @@
+public class IntroActividad1 {
+    public static void main(String[] args)  {     
+        String nombre = "David";
+        boolean bandera = true;
+        char caracter = '&';
+        int edad = 34;
+        long numeroGrande = 249856224;
+        float numeroConComa = 3.14f; // AGREGAR LA F AL FINAL
+        double numeroConComaDoble = 3.141592653589793238462643;
+
+        System.out.println("Bienvenido a tu curso en Java");
+        System.out.println(nombre + " tiene "+ edad + " años " + caracter + " esta retomando la sintaxis de Java");
+        
+    }
+
+}
