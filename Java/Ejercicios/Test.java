f30801a8bf7dcbf754dbc2d404f96b2736e73611
David Rodríguez <rdavid.97355@gmail.com>
2024-10-10 00:13:51 -0300

    ejercicios clase estructuras condicionales II: Switch

--- Java/Ejercicios/Test.java
+++ Java/Ejercicios/Test.java
@@ -2,7 +2,10 @@ package Java.Ejercicios;
 import Java.Ejercicios.EstructurasDeControl.*;
 
 public class Test {
+
+   
+
     public static void main(String[] args) {   
-        EjerciciosEstucturasCondicionales.edades();
+        EjerciciosEstructurasCondicionalesII.ultimoComp();
     }
 }
