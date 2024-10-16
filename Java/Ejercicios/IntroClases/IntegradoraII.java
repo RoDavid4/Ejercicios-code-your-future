6553f09270e69f85e870fbf172ae5667bdeb4598
David Rodríguez <rdavid.97355@gmail.com>
2024-10-16 19:13:04 -0300

    test

---
+++ Java/Ejercicios/IntroClases/IntegradoraII.java
@@ -0,0 +1,53 @@
+package Java.Ejercicios.IntroClases;
+
+import java.util.Scanner;
+
+public class IntegradoraII {
+
+    public static void convUnidades() {
+        /*
+         * Actividad: Conversión de unidades de temperatura
+         * Escribe un programa que cumpla con las siguientes condiciones:
+         * 
+         * 1. Solicita al usuario ingresar una temperatura y una unidad de medida:
+         * Celsius (C) o Fahrenheit (F).
+         * 
+         * 2. Convierte la temperatura a la otra unidad de medida y muestra el resultado
+         * en pantalla.
+         * 
+         * De Celsius a Fahrenheit De Fahrenheit a Celsius
+         * [°F] = ([°C] × 9 ⁄ 5) + 32 [°C] = ([°F] − 32) × 5 ⁄ 9
+         * 3. El programa debe manejar al menos las siguientes situaciones:
+         * 
+         * Si ingresa una unidad de medida inválida, mostrará un mensaje de error.
+         * 
+         * Si ingresa una temperatura no válida, mostrará un mensaje de error.
+         */
+
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+    public static void validContra(){
+    /*
+     * Actividad:Validación de contraseña
+     * Escribe un programa que solicite al usuario que ingrese una contraseña y
+     * verifique si cumple con los siguientes criterios para considerarse segura:
+     * 
+     * La contraseña debe tener al menos 8 caracteres.
+     * 
+     * La contraseña debe contener al menos una letra mayúscula y una letra
+     * minúscula.
+     * 
+     * La contraseña debe contener al menos un número.
+     * 
+     * La contraseña debe contener al menos un carácter especial (por ejemplo, !, @,
+     * #, $).
+     * 
+     * El programa debe mostrar un mensaje indicando si la contraseña es segura o
+     * no, según los criterios establecidos. Puedes utilizar métodos de la clase
+     * String para verificar cada uno de los criterios.
+     */
+
+    }
+}
