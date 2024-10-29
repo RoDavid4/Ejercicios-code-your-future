ae28a9ec83a848d9d3ea2de93b32db3c9619b8bb
David Rodríguez <rdavid.97355@gmail.com>
2024-10-29 20:56:02 -0300

    Clase del día 29/10

--- Java/Ejercicios/Test.java
+++ Java/Ejercicios/Test.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Test {
 
     public static void main(String[] args) {
-        IntegradoraIII.numPrimos();
+        
+        IntegradoraIV.primosPlus();
     }
 }
