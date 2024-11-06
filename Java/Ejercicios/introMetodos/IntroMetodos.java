cd66e153fd40ad5f217fb9c603ce96c019007943
David Rodríguez <rdavid.97355@gmail.com>
2024-11-06 17:07:23 -0300

    ejercicios clase 05/11

--- Java/Ejercicios/introMetodos/IntroMetodos.java
+++ Java/Ejercicios/introMetodos/IntroMetodos.java
@@ -43,48 +43,29 @@ public class IntroMetodos {
      * datos necesarios.
      */
 
+    public static String ingreseNombre() {
+        System.out.println("Ingrese un nombre: ");
+        String nombre = sc.nextLine();
+        return nombre;
 
-
-public static void main(String[] args) {
-System.out.println("\033\143");
-
-imprimeNombre(ingreseNombre(), ingreseEdad());
-sc.close();
-}
-
-public static String ingreseNombre() {
-System.out.println("Ingrese un nombre: ");
-String nombre = sc.nextLine();
-return nombre;
-
-}
-
-public static int ingreseEdad() {
-System.out.println("Ingrese su edad: ");
-return sc.nextInt();
-
-}
-
-import java.util.Scanner;
-
-public class Test {
-
-    public static void main(String[] args) {
-        /*
-         * 1. Tabla de multiplicar
-         * 
-         * En esta actividad, vas a crear un programa que solicita al usuario que
-         * ingrese un número (incluida la lógica en un método llamado pedirNumero()) y
-         * luego imprime la tabla de multiplicar de ese número del 1 al 10 (incluida la
-         * lógica en un método llamado imprimirTablaMultiplicar()). Para ello,
-         * implementarás un método que reciba el número ingresado por el usuario y que
-         * imprima la tabla de multiplicar correspondiente.
-         */
-        Scanner sc = new Scanner(System.in);
-        System.out.println("\033\143");
-        imprimirTablaMultiplicar(pedirNumero(sc));
-        sc.close();
     }
+
+    public static int ingreseEdad() {
+        System.out.println("Ingrese su edad: ");
+        return sc.nextInt();
+
+    }
+
+    /*
+     * 1. Tabla de multiplicar
+     * 
+     * En esta actividad, vas a crear un programa que solicita al usuario que
+     * ingrese un número (incluida la lógica en un método llamado pedirNumero()) y
+     * luego imprime la tabla de multiplicar de ese número del 1 al 10 (incluida la
+     * lógica en un método llamado imprimirTablaMultiplicar()). Para ello,
+     * implementarás un método que reciba el número ingresado por el usuario y que
+     * imprima la tabla de multiplicar correspondiente.
+     */
 
     public static int pedirNumero(Scanner sc) {
 
@@ -99,8 +80,36 @@ public class Test {
         }
     }
 
-}
-    
+    /*
+     * 2. Tabla de multiplicar 2.0
+     * 
+     * En esta actividad, vas a crear un programa que solicita al usuario que
+     * ingrese 3 números (incluida la lógica en un método llamado pedirNumeros()) y
+     * luego imprime la tabla de multiplicar de cada uno de ellos 1 al 10 (incluida
+     * la lógica en un método llamado imprimirTablasMultiplicar(int[] numeros)).
+     * Para ello, implementarás un un array para almacenar los números ingresados y
+     * luego invocarás el método para cada uno de ellos.
+     */
 
-    
+    public static int[] pedirNumeros(Scanner sc) {
+        int[] arreglo = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("\n Ingrese un numero ");
+            arreglo[i] = sc.nextInt();
+        }
+
+        return arreglo;
+    }
+
+    public static void imprimirTablaMultiplicar(int[] numeros) {
+
+        for (int j = 0; j < numeros.length; j++) {
+            System.out.println("\n Tabla de multiplicar del " + numeros[j] + ":");
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(numeros[j] + " x " + i + " = " + numeros[j] * i);
+            }
+        }
+    }
+
 }
